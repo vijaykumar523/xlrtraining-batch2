ae47c42f384205c30af36ba3fcdde5341ce8cda4
vijaykumar523 <2311cs010718@mallareddyuniversity.ac.in>
2025-01-02 12:17:09 +0530

    Add files via upload
    displaying numbers 1 to 100

---
+++ DisplayNumbers.java
@@ -0,0 +1,8 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        // Loop through numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);  // Print the current number
+        }
+    }
+}
