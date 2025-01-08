dd65996343cd5abd0250e2c07b8c84e56b7dc553
vijaykumar523 <2311cs010718@mallareddyuniversity.ac.in>
2025-01-08 10:15:58 +0530

    Add files via upload

---
+++ MMultiplesOfHundred.java
@@ -0,0 +1,8 @@
+public class MMultiplesOfHundred {
+    public static void main(String[] args) {
+        int limit = 10; // Change this value to get more multiples
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(100 * i);
+        }
+    }
+}
